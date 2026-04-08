f2d955c216b965b80a1bf79c76543e70515be7d0
Dzung Nguyen <dzung_min@yahoo.com>
2026-04-08 12:14:55 +0900

    first commit

---
+++ src/test/java/io/dzung/taskmanager/TestTaskmanagerApplication.java
@@ -0,0 +1,11 @@
+package io.dzung.taskmanager;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestTaskmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(TaskmanagerApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
